e1c1d81976966a5f648ed0ad70b3a2058a0f97a0
Tomáš Pružina <pruzinat@gmail.com>
2013-04-28 17:59:39 +0200

    Commented my autostart stuff 	modified:   settings/startup.lua

--- settings/startup.lua
+++ settings/startup.lua
@@ -1,8 +1,7 @@
 require("myrc.run_once")
 local awful = require("awful")
 
-run_once("firefox")
-run_once("pidgin")
-run_once("synclient tapbutton1=1")
-run_once("urxvtd","urxvtd --quiet --opendisplay --fork")
-run_once("/home/tomas/projects/spreload/spreload /home/tomas/.mozilla/firefox/new")
+--run_once("firefox")
+--run_once("pidgin")
+--run_once("synclient tapbutton1=1")
+--run_once("urxvtd","urxvtd --quiet --opendisplay --fork")
