c23c4680de0377b4a35220300a2689a785b3ed0b
Tomáš Pružina <pruzinat@gmail.com>
2013-04-28 17:58:04 +0200

    Modified startup apps 	modified:   settings/startup.lua

--- settings/startup.lua
+++ settings/startup.lua
@@ -1,12 +1,6 @@
 require("myrc.run_once")
 local awful = require("awful")
 
---run_once("firefox")
+run_once("firefox")
 run_once("urxvtd","urxvtd --quiet --opendisplay --fork")
 
-awful.util.spawn_with_shell("synclient tapbutton1=1")
-
-awful.util.spawn_with_shell("xrandr --setprovideroutputsource modesetting NVIDIA-0")
-awful.util.spawn_with_shell("sleep 1 && xrandr --auto")
-awful.util.spawn_with_shell("xrandr -q")
-awful.util.spawn_with_shell("xrandr --listproviders")
