9560a7ecfcb394d741d9222c202fbca0a4f3a823
Tomáš Pružina <pruzinat@gmail.com>
2013-05-12 09:33:05 +0200

    fixed "inversed" values on backlight up/down 	modified:   settings/custom_binds.lua

--- settings/custom_binds.lua
+++ settings/custom_binds.lua
@@ -5,8 +5,8 @@ globalkeys = awful.util.table.join(globalkeys,
     --MULTIMEDIA KEYS
     awful.key({ }, "XF86AudioRaiseVolume", function () awful.util.spawn(raise_volume_cmd) end),
     awful.key({ }, "XF86AudioLowerVolume", function () awful.util.spawn(lower_volume_cmd) end),
-    awful.key({ }, "XF86MonBrightnessDown", function () awful.util.spawn(brightness_cmd .. "-inc 10") end),
-    awful.key({ }, "XF86MonBrightnessUp", function () awful.util.spawn(brightness_cmd .. " -dec 10") end),
+    awful.key({ }, "XF86MonBrightnessDown", function () awful.util.spawn(brightness_cmd .. " -dec 10") end),
+    awful.key({ }, "XF86MonBrightnessUp", function () awful.util.spawn(brightness_cmd .. " -inc 10") end),
     awful.key({ }, "XF86AudioMute", function () awful.util.spawn(mute_cmd) end),
     awful.key({ }, "XF86Launch4", function () awful.util.spawn(hibernate_cmd) end),
     --CHANGE KEYBOARD LAYOUT
